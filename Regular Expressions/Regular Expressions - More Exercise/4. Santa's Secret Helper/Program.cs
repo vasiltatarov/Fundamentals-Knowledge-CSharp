e4f1ff28fa1f9a3c2fcc03aa587245da10bba879
vasil tatarov <vasil.tatarov@abv.bg>
2020-06-06 16:32:14 +0300

    Add files via upload

---
+++ Regular Expressions/Regular Expressions - More Exercise/4. Santa's Secret Helper/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> childrens = new List<string>();
+
+        int key = int.Parse(Console.ReadLine());
+
+        string regex = @"@(?<name>[A-Za-z]+)[^@\-!:>]+?![G]!";
+
+        string message = Console.ReadLine();
+        while (message != "end")
+        {
+            string encrypted = Encrypted(message, key);
+
+            Match match = Regex.Match(encrypted, regex);
+
+            if (match.Success)
+            {
+                childrens.Add(match.Groups["name"].Value);
+            }
+
+            message = Console.ReadLine();
+        }
+
+        foreach (var item in childrens)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+
+    public static string Encrypted(string message, int key)
+    {
+        string result = "";
+
+        foreach (var item in message)
+        {
+            result += (char)(item - key);
+        }
+
+        return result;
+    }
+}
