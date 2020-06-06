1ebace428514983bd1f73a24ba44cf6be0fd29e9
vasil tatarov <vasil.tatarov@abv.bg>
2020-06-06 14:23:21 +0300

    Add files via upload

---
+++ Regular Expressions/Regular Expressions - More Exercise/2. Rage Quit/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+
+        StringBuilder result = new StringBuilder();
+
+        var uniqueSymbols = new List<char>();
+
+        string regex = @"([\D]+)([\d]+)";
+
+        var match = Regex.Matches(text, regex);
+
+        foreach (Match item in match)
+        {
+            string symbols = item.Groups[1].Value;
+            int repeat = int.Parse(item.Groups[2].Value);
+
+            for (int i = 0; i < repeat; i++)
+            {
+                result.Append(symbols.ToUpper());
+            }
+
+            foreach (var symbol in symbols.ToUpper())
+            {
+                if (!uniqueSymbols.Contains(symbol) && !char.IsNumber(symbol))
+                {
+                    uniqueSymbols.Add(symbol);
+                }
+            }
+        }
+
+        Console.WriteLine($"Unique symbols used: {uniqueSymbols.Count}");
+        Console.WriteLine(result);
+    }
+}
