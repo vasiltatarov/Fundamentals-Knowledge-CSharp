a299abd2d48c5fcb57d041928d2c948445380654
vasil tatarov <vasil.tatarov@abv.bg>
2020-06-21 21:48:58 +0300

    Add files via upload

---
+++ Exams/Additional Retake Exam - 10 January 2019 Part I/01. Santa's Cookies/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _01._Santa_s_Cookies
+{
+    class Program
+    {
+        static void Main()
+        {
+            int batches = int.Parse(Console.ReadLine());
+            int totalBoxes = 0;
+
+            for (int i = 0; i < batches; i++)
+            {
+                int FlourInGrams = int.Parse(Console.ReadLine());
+                int sugarInGrams = int.Parse(Console.ReadLine());
+                int cocoaInGrams = int.Parse(Console.ReadLine());
+
+                int flourCups = FlourInGrams / 140;
+                int sugarSpoons = sugarInGrams / 20;
+                int cocoaSpoons = cocoaInGrams / 10;
+
+                if (flourCups <= 0 || sugarSpoons <= 0 || cocoaSpoons <= 0)
+                {
+                    Console.WriteLine("Ingredients are not enough for a box of cookies.");
+                    continue;
+                }
+
+                int totalCookiePerBake = (140 + 10 + 20) * (Math.Min(flourCups, Math.Min(sugarSpoons, cocoaSpoons))) / 25;
+
+                int boxesOfCookiesPerCurrentBake = totalCookiePerBake / 5;
+
+                Console.WriteLine($"Boxes of cookies: {boxesOfCookiesPerCurrentBake}");
+
+                totalBoxes += boxesOfCookiesPerCurrentBake;
+            }
+
+            Console.WriteLine($"Total boxes: {totalBoxes}");
+        }
+    }
+}
