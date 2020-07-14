9e7444bfb2f3bb0103d03ebda3d08c2c4fddfce7
vasil tatarov <vasil.tatarov@abv.bg>
2020-07-14 14:33:48 +0300

    Add files via upload

---
+++ Associative Arrays/Associative Arrays - More Exercise/04. Snowwhite/Dwarf.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    public class Dwarf
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Physics { get; set; }
+
+        public Dwarf(string name, string color, int physics)
+        {
+            Name = name;
+            Color = color;
+            Physics = physics;
+        }
+
+    }
+}
---
+++ Associative Arrays/Associative Arrays - More Exercise/04. Snowwhite/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Dwarf> dwarfs = new List<Dwarf>();
+
+            ReadInput(dwarfs);
+            PrintDwarfs(dwarfs);
+        }
+
+        private static void PrintDwarfs(List<Dwarf> dwarfs)
+        {
+            foreach (Dwarf dwarf in dwarfs.OrderByDescending(x => x.Physics)
+                            .ThenByDescending(x => dwarfs.Count(y => y.Color == x.Color)))
+            {
+                Console.WriteLine($"({dwarf.Color}) {dwarf.Name} <-> {dwarf.Physics}");
+            }
+        }
+
+        private static void ReadInput(List<Dwarf> dwarfs)
+        {
+            string command = Console.ReadLine();
+            while (command != "Once upon a time")
+            {
+                string[] inputs = command.Split(" <:> ");
+                string name = inputs[0];
+                string color = inputs[1];
+                int physics = int.Parse(inputs[2]);
+
+                Dwarf repeat = dwarfs.Find(x => x.Name == name && x.Color == color);
+                if (repeat != null)
+                {
+                    repeat.Physics = Math.Max(repeat.Physics, physics);
+                }
+                else
+                {
+                    Dwarf dwarf = new Dwarf(name, color, physics);
+                    dwarfs.Add(dwarf);
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
