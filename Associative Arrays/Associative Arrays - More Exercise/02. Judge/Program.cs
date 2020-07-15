771ad92b66ac8eaa8ef1eba5d376849baa2c7051
vasil tatarov <vasil.tatarov@abv.bg>
2020-07-15 23:18:02 +0300

    Update Program.cs

--- Associative Arrays/Associative Arrays - More Exercise/02. Judge/Program.cs
+++ Associative Arrays/Associative Arrays - More Exercise/02. Judge/Program.cs
@@ -8,107 +8,81 @@ namespace Problem_01
     {
         static void Main()
         {
-            var contests = new Dictionary<string, string>();
-            var students = new SortedDictionary<string, Dictionary<string, int>>();
-            
-            ReadContests(contests);
-            ReadStudentsInfo(contests, students);
-            PrintBestStudent(students);
-            PrintStudents(students);
-        }
+            var users = new Dictionary<string, Dictionary<string, int>>();
+            var individualStatistics = new Dictionary<string, int>();
 
-        private static void PrintStudents(SortedDictionary<string, Dictionary<string, int>> students)
-        {
-            Console.WriteLine("Ranking:");
-
-            foreach (var kvp in students)
+            string command = Console.ReadLine();
+            while (command != "no more time")
             {
-                Console.WriteLine(kvp.Key);
+                string[] line = command.Split(" -> ");
+                string username = line[0];
+                string contest = line[1];
+                int points = int.Parse(line[2]);
+                bool itMustSum = false;
 
-                foreach (var contest in kvp.Value.OrderByDescending(x => x.Value))
+                if (!(username.Contains(" ") || contest.Contains(" ")))
                 {
-                    Console.WriteLine($"#  {contest.Key} -> {contest.Value}");
-                }
-            }
-        }
-
-        private static void PrintBestStudent(SortedDictionary<string, Dictionary<string, int>> students)
-        {
-            var user = "";
-            int maxPoint = 0;
-
-            foreach (var kvp in students)
-            {
-                int currentPoints = 0;
-
-                foreach (var point in kvp.Value)
-                {
-                    currentPoints += point.Value;
-                }
-
-                if (currentPoints > maxPoint)
-                {
-                    maxPoint = currentPoints;
-                    user = kvp.Key;
-                }
-            }
-
-            Console.WriteLine($"Best candidate is {user} with total {maxPoint} points.");
-        }
-
-        private static void ReadStudentsInfo(Dictionary<string, string> contests, SortedDictionary<string, Dictionary<string, int>> students)
-        {
-            while (true)
-            {
-                var contestInfo = Console.ReadLine().Split("=>", StringSplitOptions.RemoveEmptyEntries);
-
-                if (contestInfo[0] == "end of submissions")
-                {
-                    break;
-                }
-
-                var contest = contestInfo[0];
-                var password = contestInfo[1];
-                var username = contestInfo[2];
-                var points = int.Parse(contestInfo[3]);
-
-                if ((contests.ContainsKey(contest)) && (contests[contest] == password))
-                {
-                    if (!students.ContainsKey(username))
+                    if (users.ContainsKey(contest))
                     {
-                        students.Add(username, new Dictionary<string, int>());
+                        if (users[contest].ContainsKey(username))
+                        {
+                            int currentPoints = users[contest][username];
+
+                            if (currentPoints < points)
+                            {
+                                users[contest][username] = points;
+                                points -= currentPoints;
+
+                                itMustSum = true;
+                            }
+                        }
+                        else
+                        {
+                            users[contest].Add(username, points);
+                            itMustSum = true;
+                        }
+                    }
+                    else
+                    {
+                        users.Add(contest, new Dictionary<string, int>());
+                        users[contest].Add(username, points);
+
+                        itMustSum = true;
                     }
 
-                    if (!students[username].ContainsKey(contest))
+                    if (!individualStatistics.ContainsKey(username))
                     {
-                        students[username].Add(contest, points);
+                        individualStatistics[username] = 0;
                     }
-                    else if (students[username][contest] < points)
+
+                    if (itMustSum)
                     {
-                        students[username][contest] = points;
+                        individualStatistics[username] += points;
                     }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var contest in users)
+            {
+                Console.WriteLine($"{contest.Key}: {contest.Value.Keys.Count} participants");
+
+                int position = 1;
+
+                foreach (var user in contest.Value.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"{position++}. {user.Key} <::> {user.Value}");
                 }
             }
-        }
 
-        private static void ReadContests(Dictionary<string, string> contests)
-        {
-            while (true)
+            Console.WriteLine("Individual standings:");
+
+            int positions = 1;
+
+            foreach (var statistics in individualStatistics.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
             {
-                var contestInfo = Console.ReadLine().Split(":", StringSplitOptions.RemoveEmptyEntries);
-
-                if (contestInfo[0] == "end of contests")
-                {
-                    break;
-                }
-
-                var contest = contestInfo[0];
-                var contestPoints = contestInfo[1];
-
-                if (!contests.ContainsKey(contest))
-                {
-                    contests.Add(contest, contestPoints);
-                }
+                Console.WriteLine($"{positions++}. {statistics.Key} -> {statistics.Value}");
             }
         }
     }
