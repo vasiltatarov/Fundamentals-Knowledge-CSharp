d3c595bcf6267f2d71d67a7b674df74bd6c9f337
vasil tatarov <vasil.tatarov@abv.bg>
2020-07-22 23:57:33 +0300

    Add files via upload

---
+++ Regular Expressions/RegularExpressionsLab/03.MatchDates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03.MatchDates
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var pattern = @"\b(?<day>\d{2})([-|.|\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+
+            var matchedDates = Regex.Matches(input, pattern);
+
+            foreach (Match date in matchedDates)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
