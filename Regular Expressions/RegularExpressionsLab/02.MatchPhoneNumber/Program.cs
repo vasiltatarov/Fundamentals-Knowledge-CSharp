d59b6076e8833e66af7407aa35d6ed17371c71dd
vasil tatarov <vasil.tatarov@abv.bg>
2020-07-22 23:56:56 +0300

    Add files via upload

---
+++ Regular Expressions/RegularExpressionsLab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var pattern = @"\+359([-|| ])2\1\d{3}\1\d{4}\b";
+
+            var matchedPhones = Regex.Matches(input, pattern);
+
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
