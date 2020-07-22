cc6fbe7c152e9f5bfb34366e6cdb0969a1717515
vasil tatarov <vasil.tatarov@abv.bg>
2020-07-22 23:55:11 +0300

    Add files via upload

---
+++ Regular Expressions/RegularExpressionsLab/01.MatchFullName/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            var matchedNames = Regex.Matches(input, pattern);
+
+            foreach (var name in matchedNames)
+            {
+                Console.Write(name + " ");
+            }
+        }
+    }
+}
